15d02121bbdd8d224f44b9bc989fe82a4a6511f7
chikatsi-Joel <kappachikatsi@gmail.com>
2024-02-04 17:35:06 +0100

    test

--- logique_metier/backbone/backbone_srt.py
+++ logique_metier/backbone/backbone_srt.py
@@ -1,4 +1,4 @@
-import logique_metier.backbone.whisper_timestamped as whisper
+import whisper_timestamped as whisper
 from datetime import timedelta
 import random, string
 from logique_metier.backbone import audio
