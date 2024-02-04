a6379e0e74b9586a18f357a7ee4462e2f971046c
chikatsi-Joel <kappachikatsi@gmail.com>
2024-02-04 16:13:34 +0100

    branc

--- Admin/components/utils.py
+++
@@ -1,8 +0,0 @@
-import random
-
-class color :
-    colors = [(42, 134, 252 ), (179, 42, 252 ), (252, 102, 42 ), (42, 105, 252 ), (164, 124, 239 ), (124, 239, 124 )]
-
-    @classmethod
-    def get_color(cls, nbre : int) :
-        return cls.colors[:nbre]
